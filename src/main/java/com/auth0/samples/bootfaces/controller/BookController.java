5df4c3a0d70fee83eab37ada5a7c2218599ab1a0
bruno.krebs <krebs.bruno@gmail.com>
2017-04-28 17:50:07 -0300

    Adding Rewrite

--- src/main/java/com/auth0/samples/bootfaces/controller/BookController.java
+++ src/main/java/com/auth0/samples/bootfaces/controller/BookController.java
@@ -2,6 +2,7 @@ package com.auth0.samples.bootfaces.controller;
 
 import com.auth0.samples.bootfaces.model.Book;
 import com.auth0.samples.bootfaces.persistence.BookRepository;
+import org.ocpsoft.rewrite.annotation.Join;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Component
 @Scope(value = "request")
+@Join(path = "/bookz", to="/index.jsf")
 public class BookController {
 
 	@Autowired
