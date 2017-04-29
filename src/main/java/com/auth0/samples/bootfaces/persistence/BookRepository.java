0512bd3a4cbe5d66f1b0de1472d4c28a00a5ece9
bruno.krebs <krebs.bruno@gmail.com>
2017-04-28 21:06:47 -0300

    Removing book

--- src/main/java/com/auth0/samples/bootfaces/persistence/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.auth0.samples.bootfaces.persistence;
-
-import com.auth0.samples.bootfaces.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
