3e823a8fcaf6344d748f47d8e4fed5809574a293
bruno.krebs <krebs.bruno@gmail.com>
2017-05-02 10:43:53 -0300

    Starting point

--- src/main/java/com/auth0/samples/bootfaces/persistence/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.auth0.samples.bootfaces.persistence;
-
-import com.auth0.samples.bootfaces.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
--- src/main/resources/db/migration/V0001__products.sql
+++
@@ -1,5 +0,0 @@
-create table product (
-  id identity not null,
-  name varchar (255) not null,
-  price double not null
-);
